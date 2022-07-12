ad8d53cf25e302013754a78bd949bbbe07588221
Diego582 <penaomardiego@gmail.com>
2022-07-12 10:12:29 -0300

    ejercicio 2 tp3

--- backend/basic/config/web.php
+++ backend/basic/config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'r0B7_ee7TDY_HG4--xjLNzNOh8c4q8ro',
+            'parsers' => ['application/json' => \yii\web\JsonParser::class],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,18 @@ $config = [
             ],
         ],
         'db' => $db,
-        
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
+            'rules' => [],
         ],
-        
+
+    ],
+    'modules' => [
+        'apiv1' => [
+            'class' => \app\modules\apiv1\Apiv1Module::class,
+        ],
     ],
     'params' => $params,
 ];
