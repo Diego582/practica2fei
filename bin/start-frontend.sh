f92fe3da35916e19fe2d14546912a7373dddab8a
Diego <81425000+Diego582@users.noreply.github.com>
2022-07-30 16:18:43 -0300

    Update start-frontend.sh

--- bin/start-frontend.sh
+++ bin/start-frontend.sh
@@ -13,8 +13,12 @@ echo $cantidad
 
 echo $rutaAnterior
 
-echo 'docker run --rm -v "'$ruta''/''$nombreProyecto'/frontend:/'$nombreProyecto'" -w "/'$nombreProyecto'" -it node:17.8.0-alpine3.14 sh -c "npm install -g @vue/cli && vue create ."'
+cd $nombreProyecto
 
+cd frontend
 
+echo $PWD
+
+echo docker run --rm -v $ruta'/'$nombreProyecto'/frontend:/'$nombreProyecto -w '/'$nombreProyecto -it node:17.8.0-alpine3.14 sh -c npm install -g @vue/cli && vue create .
 
 
